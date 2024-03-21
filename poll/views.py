942fa1ae1c319ed57a7dabed92908612e4ea31ad
marcoGoldChains <marcos.jimenezfernandez13@gmail.com>
2024-03-21 12:13:27 +0100

    Añadidos cambios al path() - subaplicaciones

--- poll/views.py
+++ poll/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hola mundo, este es el index de la página polls")
+
+def prueba(request):
+    return HttpResponse("Hola, prueba")
