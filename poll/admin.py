b6e85355503b6ed42a1f06dd85f587c7b5575b50
marcoGoldChains <marcos.jimenezfernandez13@gmail.com>
2024-03-24 23:32:07 +0100

    Customización del admin y fin del tutorial

--- poll/admin.py
+++ poll/admin.py
@@ -1,7 +1,27 @@
 from django.contrib import admin
 from .models import Question, Choice
+
 # Register your models here.
 
-admin.site.register(Question)
-admin.site.register(Choice)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+    
+class QuestionAdmin(admin.ModelAdmin):
+    
+    list_display = ('quest_text', 'pub_date', 'was_published_recently')
+    
+    fieldsets = [
+        (None,{'fields': ['quest_text']}),
+        ('Date information', {'fields': ['pub_date'], 
+                            'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_filter = ['pub_date']
+    search_fields = ['quest_text']
+
+admin.site.register(Question, QuestionAdmin)
+
+
+    
 
--- poll/models.py
+++ poll/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from django.utils import timezone
+from django.contrib import admin
 import datetime
 
 # Create your models here.
@@ -9,12 +10,19 @@ class Question(models.Model):
     quest_text = models.CharField(max_length = 200)
     pub_date = models.DateTimeField("date published")
     
+    @admin.display(
+        boolean = True, 
+        ordering = "pub_date",
+        description = "Published recently?"
+    )
+    
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
     def __str__(self) -> str:
         return self.quest_text
     
+  
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete = models.CASCADE)
     choice_text = models.CharField(max_length = 200)
